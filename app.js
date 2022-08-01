e01cc988481907502c18b65e1b1ed781b9de5bfb
Josué Alarcón <josuealarco@gmail.com>
2022-08-01 10:01:04 -0500

    Show Result on console

---
+++ app.js
@@ -0,0 +1,40 @@
+function getComputerChoice() {
+    const options = ["Paper", "Rock", "Scissor"]
+    const optioSelect = options[Math.floor(Math.random() * options.length)]
+    return optioSelect
+}
+
+function startGame(player, computer) {
+    const playerChoise = player.toLowerCase()
+    if (playerChoise === 'paper' && computer === 'Scissor') {
+        console.log(`You Lose! ${computer} beats ${player}`)
+    } else if (playerChoise === 'paper' && computer === 'Rock') {
+        console.log(`You Won! ${player}  beats ${computer}`)
+    } else if (playerChoise === 'rock' && computer === 'Paper') {
+        console.log(`You Lose! ${computer} beats ${player}`)
+    } else if (playerChoise === 'rock' && computer === 'Scissor') {
+        console.log(`You Won! ${player}  beats ${computer}`)
+    } else if (playerChoise === 'scissor' && computer === 'Rock') {
+        console.log(`You Lose! ${computer} beats ${player}`)
+    } else if (playerChoise === 'scissor' && computer === 'Paper') {
+        console.log(`You Won! ${player}  beats ${computer}`)
+    } else if (playerChoise === 'rock' && computer === 'Rock') {
+        console.log(`You Draft!`)
+    } else if (playerChoise === 'paper' && computer === 'Paper') {
+        console.log(`You Draft!`)
+    } else if (playerChoise === 'scissor' && computer === 'Scissor') {
+        console.log(`You Draft!`)
+    }else {
+        console.log(`Option no valida`)
+
+    }
+}
+
+function playRound () {
+    let game = prompt("Choose Paper, Scissor or Rock, if you want to stop press cancel")
+    while(game !== null){
+        startGame(game, getComputerChoice());
+        game = prompt("Choose Paper, Scissor or Rock, if you want to stop press cancel")
+    }
+}
+playRound()

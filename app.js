cd41a0c7c3eeae82ab9c4917f7a625172ede6672
Josué Alarcón <josuealarco@gmail.com>
2022-08-09 15:56:27 -0500

    Show Result and Declared a wining

--- app.js
+++ app.js
@@ -1,44 +1,74 @@
+let cPlayer = 0;
+let cComputer = 0;
 function getComputerChoice() {
     const options = ["Paper", "Rock", "Scissor"]
     const optioSelect = options[Math.floor(Math.random() * options.length)]
     return optioSelect
 }
+function showScores (cComputer,cPlayer ) {
+    scores = document.querySelector('#scores');
+    
+    while(scores.firstChild) {
+        scores.removeChild(scores.firstChild)
+    } 
 
+    const computer = document.createElement('p');
+    const player = document.createElement('p');
+    computer.textContent ="Computer: " + cComputer;
+    player.textContent = "Player: " +cPlayer;
+    scores.append(computer, player)
+
+}
 function startGame(player, computer) {
     const playerChoise = player.toLowerCase()
+    const show = document.querySelector('#show');
+
+
     if (playerChoise === 'paper' && computer === 'Scissor') {
-        console.log(`You Lose! ${computer} beats ${player}`)
+       show.textContent = `You Lose! ${computer} beats ${player}`;
+       cComputer++;
     } else if (playerChoise === 'paper' && computer === 'Rock') {
-        console.log(`You Won! ${player}  beats ${computer}`)
+       show.textContent = `You Won! ${player}  beats ${computer}`;
+       cPlayer++;
     } else if (playerChoise === 'rock' && computer === 'Paper') {
-        console.log(`You Lose! ${computer} beats ${player}`)
+       show.textContent = `You Lose! ${computer} beats ${player}`;
+       cComputer++;
+
     } else if (playerChoise === 'rock' && computer === 'Scissor') {
-        console.log(`You Won! ${player}  beats ${computer}`)
+       show.textContent = `You Won! ${player}  beats ${computer}`;
+       cPlayer++
     } else if (playerChoise === 'scissor' && computer === 'Rock') {
-        console.log(`You Lose! ${computer} beats ${player}`)
+       show.textContent = `You Lose! ${computer} beats ${player}`;
+       cComputer++;
+
     } else if (playerChoise === 'scissor' && computer === 'Paper') {
-        console.log(`You Won! ${player}  beats ${computer}`)
+       show.textContent = `You Won! ${player}  beats ${computer}`;
+       cPlayer++;
     } else if (playerChoise === 'rock' && computer === 'Rock') {
-        console.log(`You Draft!`)
+       show.textContent = `You Draft!`
     } else if (playerChoise === 'paper' && computer === 'Paper') {
-        console.log(`You Draft!`)
+       show.textContent = `You Draft!`
     } else if (playerChoise === 'scissor' && computer === 'Scissor') {
-        console.log(`You Draft!`)
+       show.textContent = `You Draft!`
     }else {
-        console.log(`Option no valida`)
+       show.textContent = `Option no valida`
+    }
+    showScores(cComputer,cPlayer)
+    if(cComputer == 5) {
+        show.innerHTML+="<br>The Machine has Won! We will conquer the World!"
+    }else if(cPlayer == 5) {
+        show.innerHTML+= "<br>Your Won!. You has stopped the conquer of the World"
 
     }
-}
 
-function playRound () {
-    console.log("click")
-/*     let game = prompt("Choose Paper, Scissor or Rock, if you want to stop press cancel")
-    while(game !== null){
-        startGame(game, getComputerChoice());
-        game = prompt("Choose Paper, Scissor or Rock, if you want to stop press cancel")
-    } */
+}
+showScores(cComputer,cPlayer)
+function playRound (value) {
+    startGame(value, getComputerChoice());
+
+ 
 }
 const btns = document.querySelectorAll('button');
 btns.forEach(btn => {
-    btn.addEventListener('click', playRound)
+    btn.addEventListener('click', e => playRound(e.target.value))
 })
